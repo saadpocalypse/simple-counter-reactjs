b811b7a91f050a98637f1e04dc7d3651a85c90db
Ahmad Saad <64619851+saadpocalypse@users.noreply.github.com>
2023-01-17 17:31:28 +0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,9 @@
 import './App.css';
 
-import { useState } from "react";
-
 export default function App() {
-  const [count, setCount] = useState(0);
-
-  // function Decrement() {
-  //   return setCount(count-1);
-  // }
-  function Decrement() {
-    setCount((prevCount) => prevCount - 1);
-  }
-
-  function Increment() {
-    setCount((prevCount) => prevCount + 1);
-  }
-
-  // function Increment() {
-  //   return setCount(count+1);
-  // }
-
-  function Reset() {
-    setCount(0);
-  }
-
   return (
     <div className="App">
-      <h1 className="header">My Counter App</h1>
-      <p className="count">{count}</p>
-      <button onClick={Decrement}>Decrement</button>
-      <button onClick={Reset}>Reset</button>
-      <button onClick={Increment}>Increment</button>
+      <h1>Hello world!</h1>
     </div>
   );
 }
